797a53f0f8da7f6549c1d2d0d8486fc4e07b1525
shuai <shuaitong963@gmail.com>
2020-06-29 15:24:55 -0700

    refactor

---
+++ shuai/microsoft/MinSwap/Solution.java
@@ -0,0 +1,87 @@
+import java.util.*;
+
+public class Solution {
+
+    public static int minSwap(String str){
+        char[] chs = str.toCharArray();
+        int count = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for(char ch : chs){
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        char midCh = '0';
+        int midCount = 0;
+        int mid = chs.length / 2;
+        for(char ch: map.keySet()){
+            int freq = map.get(ch);
+            if(freq % 2 == 1){
+                midCh = ch;
+                midCount ++;
+            }
+        }
+        if(midCount > 1){
+            return -1;
+        }
+        if(midCount != 0){
+            int midIdx = -1;
+            for(int i = 0; i < chs.length; i++){
+                if(chs[i] == midCh){
+                    midIdx = i;
+                    break;
+                }
+            }
+            
+            swapAtoB(chs, midIdx, mid);
+            count += Math.abs(mid - midIdx);
+        }
+        //System.out.println(new String(chs));
+        int lo = 0;
+        int hi = chs.length - 1;
+        while(lo < mid){
+            char curr = chs[lo];
+            int tmp = hi;
+            while(tmp >= lo){
+                //System.out.println(chs[tmp] + " " + tmp);
+                if(chs[tmp] == curr){
+                    break;
+                }
+                
+                tmp --;
+            }
+            swapAtoB(chs, tmp, hi);
+            count += (hi - tmp);
+            hi --;
+            lo ++;
+        }
+        return count;
+    }
+    public static void swapAtoB(char[] input, int a, int b){
+        if(a < b){
+            for(int i = a; i < b; i++){
+                swap(input, i, i+1);
+            }
+            return;
+        }
+        if(a > b){
+            for(int i = a; i > b; i--){
+                swap(input, i, i-1);
+            }
+            return;
+        }
+        
+        
+    }
+    public static void swap(char[] input, int i, int j){
+        char tmp = input[i];
+        input[i] = input[j];
+        input[j] = tmp;
+    }
+
+    public static void main(String[] args){
+        String[] input = {"mamad", "opisnd", "ntiin"};
+        for(String s : input){
+            System.out.println(minSwap(s));
+        }
+    }
+    
+}
--- shuai/microsoft/OA5/Solution.java
+++ shuai/microsoft/OA5/Solution.java
@@ -11,7 +11,7 @@ public class Solution {
         char[] chs = input.toCharArray();
         // 双指针；　count : 记录前一个字符出现的次数
         int start = 0, end = 0, count = 0;
-        // 记录符合条件的最长 substring 对应的区间 // [0,1)
+        // 记录符合条件的最长 substring 对应的区间
         int[] subStrRange = {0,1};
 
         // 记录前一个出现的字符， 初始化为 idx = 0 处的字符
@@ -25,7 +25,7 @@ public class Solution {
                   count ++;
                   // 连续字符出现后：1 计算当前长度，决定是否更新 subStrRange 2 更新start 和count
                   if(count > 2){
-                    int len = end - start; //[0,1) [0, 2)]
+                    int len = end - start;
                     if(len > subStrRange[1] - subStrRange[0]){
                         subStrRange[0] = start;
                         subStrRange[1] = end;
